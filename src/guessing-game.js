3d27406de736b805cf3f0c360781d3b7baddaa72
Kisliankou Viachaslau <kislenkov.vs@gmail.com>
2022-08-16 21:52:01 +0300

    feat: task is ready

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,23 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }test
+
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        let k = Math.round((this.max + this.min) / 2);
+        return k;
     }
 
     lower() {
-
+       this.max = this.guess();
     }
 
     greater() {
-
+       this.min = this.guess();
     }
 }
 
